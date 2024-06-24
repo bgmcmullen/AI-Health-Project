21acf92d9ed9368bd4fac968c9e08aa94e606756
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-06-25 04:25:17 +0500

    Unwrap custom elements after building the DOM to keep the DOM structure clean and readable

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,9 @@ import App from './App';
 const app = async () => {
   await Splashscreen(2000);
   document.getElementById('app').appendChild(await App());
+
+  // Unwrapping my custom elements to keep DOM clean
+  $(".upload, .manually").unwrap();
 };
 
 // Load app
