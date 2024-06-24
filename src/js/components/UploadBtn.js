fdfb533608e6aaf389ffca81a9ee6f1c868f617d
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-06-25 04:35:57 +0500

    Add logic to 'Upload' button: it triggers file input click, uploads the file (temporarily used as preview image) and triggers the transition from Homepage to Uploading view

--- src/js/components/UploadBtn.js
+++ src/js/components/UploadBtn.js
@@ -9,6 +9,31 @@ class UploadBtn extends HTMLElement {
         <input id="file-input" type="file" hidden>
       </section>
     `;
+    
+    // Pass image file to the Loading page
+    const btn = this.querySelector("button");
+    const preview = $("#uploading img");
+    let url;
+
+    $(btn).on('click', () => {
+      $("#file-input").trigger("click");
+    });
+
+    $("#file-input").on('change', (e) => {
+      const [file] = e.target.files;
+      if (file) {
+        url = URL.createObjectURL(file)
+        preview.attr("src", url);
+      }
+      preview.on("load", () => {
+        URL.revokeObjectURL(url);
+        $("#homepage").fadeOut(800, () => {
+          $("#uploading").fadeIn(800);
+        });
+        // parse image
+        // analyze data
+      });
+    });
   }
 };
 
