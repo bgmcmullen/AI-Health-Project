1d0464397afdd570b161dc3cc31238ed32f1fa39
Brendan <bgmcmullen@comcast.net>
2024-08-03 22:00:12 -0700

    added comments

--- src/js/components/Splashscreen.js
+++ src/js/components/Splashscreen.js
@@ -1,21 +1,10 @@
-import Logo from '../../../public/logo.svg';
+'use strict';
 
 const Splashscreen = async (param) => {
-  // Show the splashscreen
-  // const template = document.createElement('template');
-  // template.innerHTML = `
-  //   <div id="splashscreen" class="flex-column align-items-center justify-content-center">
-  //     <img src=${Logo} class="splash-logo mb-5"/>
-  //     <div class="spinner-grow" role="status">
-  //       <span class="visually-hidden">Loading...</span>
-  //     </div>
-  //   </div>
-  // `;
-  // document.body.appendChild(template.content.cloneNode(true));
 
-  // Show the app when content loaded (with delay)
   const wait = (delay) => new Promise(resolve => setTimeout(resolve, delay));
   document.addEventListener('DOMContentLoaded', () =>
+    // wait for delay, then fade out splash screen
     wait(param).then(() => {
       $('#splashscreen').fadeOut(500, () => {
         $('#wrapper').fadeIn(500);
--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import "./import-jquery";
 import Splashscreen from './components/Splashscreen'
 
 const app = async () => {
+  // display splash screen for 2 seconds
   await Splashscreen(2000);
 };
 
